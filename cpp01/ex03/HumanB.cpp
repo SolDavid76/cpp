e1fa3cb952895deceea7b7690ab7073ac1ab8e21
SolDavid_76 <david.janusz75@gmail.com>
2023-10-03 16:44:14 +0200

    fix for 3rd correction

--- cpp01/ex03/HumanB.cpp
+++ cpp01/ex03/HumanB.cpp
@@ -6,14 +6,14 @@
 /*   By: djanusz <djanusz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 18:42:03 by djanusz           #+#    #+#             */
-/*   Updated: 2023/09/26 11:10:43 by djanusz          ###   ########.fr       */
+/*   Updated: 2023/10/03 16:41:29 by djanusz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "HumanB.hpp"
 
-HumanB::HumanB(std::string name): _name(name)
+HumanB::HumanB(std::string name): _name(name), _weapon(NULL)
 {
 }
 
@@ -33,5 +33,8 @@ void HumanB::set_Weapon(Weapon& weapon)
 
 void HumanB::attack(void)
 {
-	std::cout << this->_name << " attacks with their " << this->_weapon->get_Type() << std::endl;
+	if (this->_weapon == NULL)
+		std::cout << this->_name << " cannot attack i don't have weapon" << std::endl;
+	else
+		std::cout << this->_name << " attacks with their " << this->_weapon->get_Type() << std::endl;
 }
--- cpp01/ex03/main.cpp
+++ cpp01/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: djanusz <djanusz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 15:48:53 by djanusz           #+#    #+#             */
-/*   Updated: 2023/09/25 14:57:01 by djanusz          ###   ########.fr       */
+/*   Updated: 2023/10/03 16:42:11 by djanusz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,6 @@
 #include "HumanB.hpp"
 #include "Weapon.hpp"
 #include <iostream>
-
-// int main(void)
-// {
-// 	Weapon weapon1;
-// 	Weapon weapon2;
-
-// 	weapon1.set_type("Vandal");
-// 	weapon2.set_type("Phantom");
-// 	HumanA human1("bob", weapon1);
-// 	HumanB human2;
-
-// 	human1.set_name("bob");
-// 	human2.set_name("boby");
-// 	human2.set_weapon(weapon2);
-
-// 	human1.attack();
-// 	human2.attack();
-// }
-
 
 int main()
 {
