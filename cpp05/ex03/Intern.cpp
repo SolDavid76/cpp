cc60472803887f10c2d4cc36aea2211f101e3332
SolDavid_76 <david.janusz75@gmail.com>
2023-10-31 12:28:46 +0100

    fix

--- cpp05/ex03/Intern.cpp
+++ cpp05/ex03/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: djanusz <djanusz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 15:41:16 by djanusz           #+#    #+#             */
-/*   Updated: 2023/10/30 12:52:28 by djanusz          ###   ########.fr       */
+/*   Updated: 2023/10/31 12:28:28 by djanusz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,5 +48,7 @@ AForm* Intern::makeForm(std::string name, std::string target)
 	}
 	if (res == NULL)
 		std::cout << "Intern cannot find " << name << " form" << std::endl;
+	else
+		std::cout << "Intern creates " << name << std::endl;
 	return (res);
 }
