d7dc316bc2d2265d4230563e99bbdf2412b6a787
SolDavid_76 <david.janusz75@gmail.com>
2023-11-15 12:41:15 +0100

    little fix on iter

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: djanusz <djanusz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 13:35:48 by djanusz           #+#    #+#             */
-/*   Updated: 2023/11/03 13:57:07 by djanusz          ###   ########.fr       */
+/*   Updated: 2023/11/15 12:40:41 by djanusz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@
 # include <cstddef>
 
 template<typename T>
-void iter(T* array, size_t len, T (*f)(T))
+void iter(T* array, size_t len, void(*f)(T))
 {
 	for (size_t i = 0; i < len; i++)
-		array[i] = f(array[i]);
+		f(array[i]);
 }
 
 #endif
