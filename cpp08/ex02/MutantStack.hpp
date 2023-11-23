c679787321978bee26b62e2910b023b3df8b3278
SolDavid_76 <david.janusz75@gmail.com>
2023-11-23 12:02:42 +0100

    others iterators

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: djanusz <djanusz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 16:55:56 by djanusz           #+#    #+#             */
-/*   Updated: 2023/11/09 14:29:04 by djanusz          ###   ########.fr       */
+/*   Updated: 2023/11/15 16:45:56 by djanusz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,9 @@ class MutantStack: public std::stack<T>
 {
 	public:
 		typedef typename MutantStack<T>::container_type::iterator iterator;
+		typedef typename MutantStack<T>::container_type::const_iterator const_iterator;
+		typedef typename MutantStack<T>::container_type::reverse_iterator reverse_iterator;
+		typedef typename MutantStack<T>::container_type::const_reverse_iterator const_reverse_iterator;
 		MutantStack(void);
 		MutantStack(MutantStack const& src);
 		MutantStack& operator=(MutantStack const& src);
@@ -29,6 +32,12 @@ class MutantStack: public std::stack<T>
 
 		iterator begin(void);
 		iterator end(void);
+		const_iterator cbegin(void);
+		const_iterator cend(void);
+		reverse_iterator rbegin(void);
+		reverse_iterator rend(void);
+		const_reverse_iterator crbegin(void);
+		const_reverse_iterator crend(void);
 };
 
 template <typename T>
@@ -64,6 +73,42 @@ template <typename T>
 typename MutantStack<T>::iterator MutantStack<T>::end(void)
 {
 	return (this->c.end());
+}
+
+template <typename T>
+typename MutantStack<T>::const_iterator MutantStack<T>::cbegin(void)
+{
+	return (this->c.cbegin());
+}
+
+template <typename T>
+typename MutantStack<T>::const_iterator MutantStack<T>::cend(void)
+{
+	return (this->c.cend());
+}
+
+template <typename T>
+typename MutantStack<T>::reverse_iterator MutantStack<T>::rbegin(void)
+{
+	return (this->c.regin());
+}
+
+template <typename T>
+typename MutantStack<T>::reverse_iterator MutantStack<T>::rend(void)
+{
+	return (this->c.rend());
+}
+
+template <typename T>
+typename MutantStack<T>::const_reverse_iterator MutantStack<T>::crbegin(void)
+{
+	return (this->c.crbegin());
+}
+
+template <typename T>
+typename MutantStack<T>::const_reverse_iterator MutantStack<T>::crend(void)
+{
+	return (this->c.crend());
 }
 
 #endif
